0a2ffc6b2891ce75d83471aca7681e012f14a871
Jenny Traer <lunalikesart@gmail.com>
2016-02-02 13:29:57 +0000

    Add validation to new course fields

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -5,9 +5,13 @@ class Instructor::CoursesController < ApplicationController
 		@course = Course.new
 	end
 
-	def create 
-		@course = current_user.courses.create(course_params)
-		redirect_to instructor_course_path(@course)
+  def create
+    @course = current_user.courses.create(course_params)
+    if @course.valid?
+      redirect_to instructor_course_path(@course)
+    else
+      render :new, status: :unprocessable_entity
+    end
 	end
 
 	def show 
