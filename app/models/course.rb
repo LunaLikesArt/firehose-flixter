94691da47f2ff0d56666d713e3fe5e37e2a5d74d
Jenny Traer <lunalikesart@gmail.com>
2016-02-07 18:02:11 +0000

    Add student enrollment funstionality to course show page

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,13 +1,14 @@
 class Course < ActiveRecord::Base
 	belongs_to :user
 	has_many :sections
+	has_many :enrollments
 	
 	validates :title, presence: true
 	validates :description, presence: true
 	validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0}
-	
+
 	mount_uploader :image, ImageUploader
-	
+
 	def price_checker
 		if self.cost == 0
 			return "Free"
@@ -15,5 +16,5 @@ class Course < ActiveRecord::Base
 			return self.cost
 		end
 	end
-	
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,40 @@
 class User < ActiveRecord::Base
 	has_many :courses
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+	has_many :enrollments
+	has_many :enrolled_courses, through: :enrollments, source: :course
+	# Include default devise modules. Others available are:
+	# :confirmable, :lockable, :timeoutable and :omniauthable
+	devise :database_authenticatable, :registerable,
+	:recoverable, :rememberable, :trackable, :validatable
+
+
+	# Version 1 (Array 'include' method)
+	# def enrolled_in?(course)
+	# 	enrolled_courses = []
+	# 	enrollments.each do |enrollment|
+	# 		enrolled_courses << enrollment.course
+	# 	end
+	# 	return enrolled_courses.include?(course)
+	# end
+
+	# Version 2 (Array 'collect' method)
+	# 	def enrolled_in?(course)
+	# 		enrolled_courses = enrollments.collect do |enrollment|
+	# 			enrollment.course
+	# 		end
+	#
+	# 		return enrolled_courses.include?(course)
+	# 	end
+
+	# Version 3 (N+1 Query)
+	# def enrolled_in?(course)
+	# 	enrolled_courses = enrollments.collect(&:course)
+	# 	return enrolled_courses.include?(course)
+	# end
+
+	# Version 4 (has_many :through relationship)
+	def enrolled_in?(course)
+		return enrolled_courses.include?(course)
+	end
+
 end
