f5d5fb4a8b15c78979f2a5f05d244ec6000f1eb5
Jenny Traer <lunalikesart@gmail.com>
2016-02-08 12:45:01 +0000

    Add before_action to ensure student is enrolled in course

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,13 +1,22 @@
 class LessonsController < ApplicationController
+	before_action :authenticate_user!
+	before_action :require_enrollment_for_current_section, only: :show
 
-	def show 
+	def show
 	end
 
 	private
-	
+
 	helper_method :current_lesson
 	def current_lesson
 		@current_lesson ||= Lesson.find(params[:id])
+	end
+
+	def require_enrollment_for_current_section
+		current_course = current_lesson.section.course
+		if current_user.enrolled_in?(current_course) != true
+			redirect_to course_path(current_course), alert: 'You have to be enrolled to see the lesson'
+		end
 	end
 
 end
