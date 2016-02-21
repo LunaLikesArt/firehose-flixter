1a412e02b5cedd2574a134c975aa6dac39d9188a
Jenny Traer <lunalikesart@gmail.com>
2016-02-21 20:12:39 +0000

    Add rank(:row_order) to instructor show page and course overview page Update routes.rb to expose endpoint to update section's position Added before_actions to make sure only instructors can update their own sections

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,31 +1,41 @@
 class Instructor::SectionsController < ApplicationController
   before_action :authenticate_user!
   before_action :require_authorized_for_current_course
-	
-	def new
-		@section = Section.new
-	end
+  before_action :require_authorized_for_current_section, only: [:update]
 
-	def create 
-		@section = current_course.sections.create(section_params)
-		redirect_to instructor_course_path(current_course)
-	end 
-	
-	private
+  def new
+    @section = Section.new
+  end
 
-	def require_authorized_for_current_course
-		if current_course.user != current_user
-			render text: "NOPE!", status: :unauthorized
-		end
-	end
+  def create
+    @section = current_course.sections.create(section_params)
+    redirect_to instructor_course_path(current_course)
+  end
 
-	helper_method :current_course
-	def current_course
-		@current_course ||= Course.find(params[:course_id])
-	end
+  def update
+    current_section.update_attributes(section_params)
+    render text: 'updated!'
+  end
 
-	def section_params 
-		params.require(:section).permit(:title)
-	end
+  private
+
+  def current_section
+    @current_section ||= Section.find(params[:id])
+  end
+
+  def require_authorized_for_current_course
+    if current_course.user != current_user
+      render text: "NOPE!", status: :unauthorized
+    end
+  end
+
+  helper_method :current_course
+  def current_course
+    @current_course ||= Course.find(params[:course_id])
+  end
+
+  def section_params
+    params.require(:section).permit(:title, :row_order_position)
+  end
 
 end
