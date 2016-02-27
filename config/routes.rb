78a096400d2a95d45e5ba2ba608b96b4d36f480e
Jenny Traer <lunalikesart@gmail.com>
2016-02-27 06:08:18 +0000

    Add static privacy page

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Flixter::Application.routes.draw do
   devise_for :users
   resource :dashboard, only: [:show]
   root 'static_pages#index'
+  get 'privacy', to: 'static_pages#privacy'
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
   end
   resources :lessons, only: [:show]
-  namespace :instructor do  
+  namespace :instructor do
     resources :lessons, only: [:update]
     resources :sections, only: [:update] do
       resources :lessons, only: [:create]
